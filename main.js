b8aaa520f9cdcf3f283747bc8c35acb3651e143b
luanluan <dangluan15112001@gmail.com>
2022-03-24 15:28:55 +0700

    update port env

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api', router)
-server.listen(4000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
